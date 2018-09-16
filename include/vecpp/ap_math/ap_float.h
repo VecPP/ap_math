681de10e1b641f8315558504dc887effa6d76580
francois <francois.chabot.dev@gmail.com>
2018-09-15 23:50:30 -0400

    added floating point comparisons

--- include/vecpp/ap_math/ap_float.h
+++ include/vecpp/ap_math/ap_float.h
@@ -22,18 +22,124 @@ namespace vecpp {
     static_assert(mantissa_bits > 1);
     static_assert(exponent_bits > 1);
     
-    constexpr Ap_float& operator+=(const Ap_float& rhs);
+
+    constexpr bool operator==(const Ap_float& rhs) const ;
+    constexpr bool operator!=(const Ap_float& rhs) const;
+    constexpr bool operator<(const Ap_float&) const;
+    constexpr bool operator<=(const Ap_float&) const;
+    constexpr bool operator>(const Ap_float&) const;
+    constexpr bool operator>=(const Ap_float&) const;
+
 
   private:
+
+    constexpr int compare(const Ap_float& rhs) const;
+    constexpr bool is_negative() const {return m_.is_negative();}
+
+    enum class State {
+      normal,
+      nan,
+      pos_inf,
+      neg_inf,
+    };
+
     // The sign of the mantissa is the sign of the value.
-    Ap_int<mantissa_bits> mantissa;
-    Ap_int<exponent_bits> exponent;
+    Ap_int<mantissa_bits> m_;
+    Ap_int<exponent_bits> e_;
+    State s_ = State::normal;
   };
 
   template<std::size_t M, std::size_t E>
-  constexpr Ap_float<M, E>& Ap_float<M, E>::operator+=(const Ap_float& rhs) {
-    return *this;
+  constexpr int Ap_float<M, E>::compare(const Ap_float& rhs) const {
+    switch(s_) {
+      case State::pos_inf:
+        return rhs.s_ == State::pos_inf ? 0 : 1;
+      case State::neg_inf:
+        return rhs.s_ == State::neg_inf ? 0 : -1;
+      default: 
+        break;
+    }
+
+    auto l_neg = is_negative();
+    auto r_neg = rhs.is_negative();
+
+    if (l_neg != r_neg) {
+      return l_neg ? -1 : 1;
+    }
+
+    auto e_cmp = e_.compare(rhs.e_);
+    if(e_cmp < 0) {
+      return l_neg ? 1 : -1;
+    }
+
+    if(e_cmp > 0) {
+      return l_neg ? -1 : 1;
+    }
+
+
+    return m_.compare(rhs.m_);
   }
+
+  template<std::size_t M, std::size_t E>
+  constexpr bool Ap_float<M, E>::operator<(const Ap_float& rhs) const {
+    if( s_ == State::nan || rhs.s_ == State::nan) {
+      return false;
+    }
+    return compare(rhs) < 0;
+  }
+
+  template<std::size_t M, std::size_t E>
+  constexpr bool Ap_float<M, E>::operator<=(const Ap_float& rhs) const {
+    if( s_ == State::nan || rhs.s_ == State::nan) {
+      return false;
+    }
+    return compare(rhs) <= 0;
+  }
+
+  template<std::size_t M, std::size_t E>
+  constexpr bool Ap_float<M, E>::operator>(const Ap_float& rhs) const {
+    if( s_ == State::nan || rhs.s_ == State::nan) {
+      return false;
+    }
+    return compare(rhs) > 0;
+  }
+
+  template<std::size_t M, std::size_t E>
+  constexpr bool Ap_float<M, E>::operator>=(const Ap_float& rhs) const {
+    if( s_ == State::nan || rhs.s_ == State::nan) {
+      return false;
+    }
+    return compare(rhs) >= 0;
+  }
+  template<std::size_t M, std::size_t E>
+  constexpr bool Ap_float<M, E>::operator==(const Ap_float& rhs) const { 
+    if(rhs.s_ != s_) {
+      return false;
+    }
+
+    switch(s_) {
+      case State::normal:
+        return rhs.m_ == rhs.m_ && e_ == rhs.e_;
+        break;
+      case State::nan:
+        return false;
+        break;
+      case State::pos_inf:
+        return rhs.s_ == State::pos_inf;
+        break;
+      case State::neg_inf:
+        return rhs.s_ == State::neg_inf;
+        break;
+    }
+    assert(false);
+    return false;
+  }
+
+  template<std::size_t M, std::size_t E>
+  constexpr bool Ap_float<M, E>::operator!=(const Ap_float& rhs) const {
+    return !(*this == rhs);
+  }
+
 }
 
 #endif
